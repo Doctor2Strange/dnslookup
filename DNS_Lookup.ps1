a26864077708b9c7991b542f3482388337ecf231
Doctor2Strange <89478879+Doctor2Strange@users.noreply.github.com>
2021-08-25 00:14:12 +0530

    Add files via upload

---
+++ DNS_Lookup.ps1
@@ -0,0 +1,24 @@
+﻿$inputpath = "C:\Powershell-Automation\URLs.txt" <#Input text file path here#>
+$outputpath = "C:\Powershell-Automation\dnslookup_output.csv" <#Output .csv file path here#>
+$NameList = Get-Content -Path $inputpath
+$ServerList = @('8.8.8.8','8.8.4.4')
+$FinalResult = @()
+foreach ($Name in $NameList) 
+{    
+$tempObj = "" | Select-Object Name,IPAddress,Status,ErrorMessage    
+try {        
+$dnsRecord = Resolve-DnsName $Name -Server $ServerList -ErrorAction Stop | Where-Object {$_.Type -eq 'A'}
+$tempObj.Name = $Name
+$tempObj.IPAddress = ($dnsRecord.IPAddress -join ', ')
+$tempObj.Status = 'DNS Entry Found'        
+$tempObj.ErrorMessage = ''    }
+catch {        
+$tempObj.Name = $Name
+$tempObj.IPAddress = ''
+$tempObj.Status = 'No DNS Entry' 
+$tempObj.ErrorMessage = $_.Exception.Message    }
+$FinalResult += $tempObj} 
+echo '*******************************************************************'
+echo '********************+++ Task Complete...!!! +++********************'
+echo '*******************************************************************'
+return $FinalResult | Export-Csv -path $outputpath -NoTypeInformation
